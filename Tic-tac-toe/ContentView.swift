fabb16751dedb79947cc875a81beec2721719124
Joe <65048288+AwkwardGentleman@users.noreply.github.com>
2021-06-25 08:58:49 +0100

    Initial Commit

---
+++ Tic-tac-toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic-tac-toe
+//
+//  Created by joe leadbeater on 25/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
